28706c853fd32bdc71e7a12627d0bbe83325264b
Noah Adams <nhadams10@uchicago.edu>
2021-04-18 15:17:03 +0000

    Created Follow Requests Table and Added Users Associations Moving along

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,21 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+
+  has_many(:follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+
+  has_many(:user_contents, { :class_name => "UserContent", :foreign_key => "owner_id", :dependent => :destroy })
+
+  has_many(:likes, { :class_name => "Like", :foreign_key => "user_id", :dependent => :destroy })
+
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "commenter_id", :dependent => :destroy })
+
+  has_many(:subscriptions, { :class_name => "Subscription", :foreign_key => "subscriber_id", :dependent => :destroy })
+
+  has_many(:subscribers, { :class_name => "Subscription", :foreign_key => "creator_id", :dependent => :destroy })
+
+  has_many(:donations, { :class_name => "Donation", :foreign_key => "donator_id", :dependent => :destroy })
+
+  has_many(:given_donations, { :class_name => "Donation", :foreign_key => "creator_id", :dependent => :destroy })
+
 end
